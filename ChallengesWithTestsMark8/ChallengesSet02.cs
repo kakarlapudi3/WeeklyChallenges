45e2b21130e8406ae3f4d0c0a16753ec87f5bd1f
Ria Kakarlapudi <ria.kakarlapudi@gmail.com>
2024-05-16 16:29:36 -0500

    initial commit

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,41 +8,99 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return Char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
             throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+             throw new NotImplementedException();
         }
 
         public bool IsNumberOdd(int num)
         {
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
             throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
+            return numbers.Min() + numbers.Max();
             throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+
+            else
+            {
+                return str2.Length;
+            }
             throw new NotImplementedException();
         }
 
         public int Sum(int[] numbers)
         {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+            }
+            return sum;
+
+
+
+
             throw new NotImplementedException();
         }
 
         public int SumEvens(int[] numbers)
         {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+
+                }
+            }
+            
+            
             throw new NotImplementedException();
         }
 
